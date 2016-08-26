5bd5c363b2212a99d041cf0a4a6797eb1822bc21
eddanieles <ed.danieles@yahoo.com>
2016-08-25 21:52:53 -0400

    able to random and prepend

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 (function($){
 
     $(document).ready(function(){
-        $('#etsy-search').bind('submit', function() {
-            terms = $('#etsy-terms').val();
-            etsyURL = "http://api.giphy.com/v1/gifs/search?q=" + terms + "&api_key=dc6zaTOxFJmzC&limit=1";
-            $('<p></p>').text('Giphy '+terms).appendTo('#etsy-images');
 
+        $('#gif_search').bind('submit', function() {
+            terms = $('#search_terms').val();
+            apiURL = "http://api.giphy.com/v1/gifs/search?q=" + terms + "&api_key=dc6zaTOxFJmzC&limit=50";
             $.ajax({
-                url: etsyURL,
+                url: apiURL,
                 type: 'GET',
                 success: function(data) {
                   console.log(data);
-                  $.each(data.data, function(i,item) {
-                      $("#etsy-images").prepend($("<img/>").attr("src", item.images.original.url));
-                  });
+                  var random = Math.ceil(Math.random()*50);
+                  var randomGifURL = data.data[random].images.original.url;
+                  var gif = `<p class="gif"><img src="${randomGifURL}" alt="" /></p>`;
+                  $("#gif_container").prepend(gif);
+                  var searchText = `<p>Giphy <em>"${terms}"</em></p>`;
+                  $("#gif_container").prepend(searchText);
                 }
             });
-
             return false;
         })
     });
