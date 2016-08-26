a0d56e612a44e0c48198a20419641af648171727
eddanieles <ed.danieles@yahoo.com>
2016-08-26 17:34:31 -0400

    added favorites

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
                   //console.log(data);
                   var random = Math.ceil(Math.random() * 100);
                   var randomGifURL = data.data[random].images.original.url;
-                  var gifSection = `<p class="terms">Giphy <em>"${terms}"</em> <i class="fa fa-heart-o" aria-hidden="true"></i></p>
+                  var gifSection = `<p class="terms">Giphy <em>"${terms}"</em> <i class="fa fa-heart" aria-hidden="true"></i></p>
                   <p class="gif"><img src="${randomGifURL}" alt="" /></p>`;
                   $("#gif_container").prepend(gifSection);
                 }
@@ -21,11 +21,32 @@
             return false;
         })
 
+        var favoritesArray = [];
         $('#gif_container').on('click', '.fa', function(e){
           var self = $(this);
           var gifFavorite = self.closest("div").find("img").attr("src");
           localStorage.setItem('favorite', gifFavorite);
+          favoritesArray.push(gifFavorite);
+          console.log(localStorage);
+          console.log(favoritesArray);
         })
+
+        $('#favorites_button').click(function(){
+          $(".jumbotron").hide();
+          $("#gif_container").hide();
+          favoritesArray.forEach(function(gif){
+            $("#favorites_page").prepend(`<p><img src=${gif} alt="" /></p>`);
+          })
+          $("#favorites_page").show();
+        })
+
+        $(window).on("hashchange", function(event){
+          showPage(location.hash);
+        })
+
+        location.hash = "#Home";
+
+
     });
 
 })(jQuery);
